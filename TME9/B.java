d8748ad2315f0e0bc84ef81bc8202697b107cca4
Ilyas <157494002+B-Ilyas@users.noreply.github.com>
2024-02-29 20:47:03 +0100

    Add files via upload

---
+++ TME9/B.java
@@ -0,0 +1,14 @@
+package pack2;
+import pack1.A;
+
+public class B {
+	
+	public String toString(){
+		return "Je suis B";
+	}
+	
+	public static void main(String[] args){
+		A a1 = new A();
+		System.out.println("Ca marche");
+	}
+}
